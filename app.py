a9103b192e22cde8561e03e3590ff3a5e0ab70c9
mahmoudmatter12 <HNU.24.363.Mahmoud-zEl-Din@hnu.edu.eg>
2024-10-17 18:36:36 +0300

    v2-test

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask , render_template
+from flask import Flask
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return render_template('index.html')
+def home():
+    return "Hello, Flask!"
+
+# Required for Vercel deployment to handle WSGI
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == '__main__':
     app.run(debug=True)
